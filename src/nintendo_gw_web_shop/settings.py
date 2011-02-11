01d813a058faae746ac47f8578935c1488c609b5
kjsaila <kjsaila@e312b277-0812-2fab-d82e-aec1ed09d4bf>
2011-02-11 07:35:33 +0000

    main settings edited

--- src/nintendo_gw_web_shop/settings.py
+++ src/nintendo_gw_web_shop/settings.py
@@ -12,7 +12,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/markku/data/applications/eclipse/workspace/nintendo_gw_web_shop/src/sqlite.db',                      # Or path to database file if using sqlite3.
+        #'NAME': '/Users/markku/data/applications/eclipse/workspace/nintendo_gw_web_shop/src/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/Users/kjsaila/Documents/workspace/nintendo_gw_web_shop_svn/src/sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -45,12 +46,12 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/Users/kjsaila/Documents/workspace/nintendo_gw_web_shop_svn/src/nintendo_gw_web_shop/static/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -81,7 +82,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/markku/data/applications/eclipse/workspace/nintendo_gw_web_shop/src/nintendo_gw_web_shop/templates'
+    #'/Users/markku/data/applications/eclipse/workspace/nintendo_gw_web_shop/src/nintendo_gw_web_shop/templates'
+    '/Users/kjsaila/Documents/workspace/nintendo_gw_web_shop_svn/src/nintendo_gw_web_shop/templates'
 )
 
 INSTALLED_APPS = (
@@ -91,6 +93,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'polls',
+    'webshop',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
