d022420cd3b260f061d539160e08d829198103b8
kjsaila <kjsaila@e312b277-0812-2fab-d82e-aec1ed09d4bf>
2011-02-21 09:39:32 +0000

    added data from db to home page

--- src/nintendo_gw_web_shop/webshop/views.py
+++ src/nintendo_gw_web_shop/webshop/views.py
@@ -5,17 +5,23 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from webshop.forms import ProductForm
-from webshop.models import ProductDetails, Product
+from webshop.models import ProductDetails, Product, Type
 from django.template.loader import get_template
 from django.template.context import Context
 from django.core.context_processors import csrf
+
 #from django.http import HttpResponse
 #from django.template import Context, loader
 #from polls.models import Choice, Poll
 
 
 def home( request ):
-	return render_to_response( 'webshop/index.html', context_instance = RequestContext( request ) )
+	types = Type.objects.all().order_by('name')
+	template = get_template('webshop/index.html')
+	variables = Context({'types':types})
+	
+	return HttpResponse(template.render(variables))
+	#return render_to_response( 'webshop/index.html', context_instance = RequestContext( request ) )
 
 def category( request ):
 	return render_to_response( 'webshop/category.html', context_instance = RequestContext( request ) )
