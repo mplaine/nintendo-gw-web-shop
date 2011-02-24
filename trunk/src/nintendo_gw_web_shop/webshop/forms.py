90a315199afe26a6f97af0be893d242d16f01e24
markku.laine <markku.laine@e312b277-0812-2fab-d82e-aec1ed09d4bf>
2011-02-24 22:57:04 +0000

    Added a base template and changed web pages to use that. Updating account information works now (1. draft).

--- trunk/src/nintendo_gw_web_shop/webshop/forms.py
+++ trunk/src/nintendo_gw_web_shop/webshop/forms.py
@@ -1,7 +1,17 @@
 from django import forms 
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
 from django.forms import widgets
 
 class ProductForm(forms.Form): 
     name        = forms.CharField(label=u'Name', max_length=50)
     price       = forms.DecimalField(label=u'Price', decimal_places=2, max_digits=6)
     quantity    = forms.IntegerField(label=u'Quantity')
+
+class MyUserChangeForm( UserChangeForm ):
+    class Meta( UserChangeForm.Meta ):
+        fields  = ( "first_name", "last_name", "email" )
+    
+    def __init__( self, *args, **kwargs ):
+        super( MyUserChangeForm, self ).__init__( *args, **kwargs )
+        del self.fields[ "username" ] # This is a declared field we really want to be removed from the form to be rendered
