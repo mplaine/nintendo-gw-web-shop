2327f56f839af8e034d56368e42a4a788d0eadb0
markku.laine <markku.laine@e312b277-0812-2fab-d82e-aec1ed09d4bf>
2011-02-26 10:04:08 +0000

    Logging in happens now on a dedicated web page. Makes things easier when working with views.py. In views.py, it is now possible to refer to urls defined in urls.py. No more inconsistency problems.

--- trunk/src/nintendo_gw_web_shop/webshop/views.py
+++ trunk/src/nintendo_gw_web_shop/webshop/views.py
@@ -1,8 +1,5 @@
-# Create your views here.
 from django.core.urlresolvers import reverse
-#from django.http import Http404
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
 from webshop.forms import ProductForm, MyUserChangeForm
 from webshop.models import SaleItem, Product, Type
@@ -12,53 +9,58 @@ from django.core.context_processors import csrf
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
- 
-#from django.http import HttpResponse
-#from django.template import Context, loader
-#from polls.models import Choice, Poll
 
 
 def home( request ):
-	if request.method == "POST":
-		form			= AuthenticationForm( data=request.POST )
-		if form.is_valid():
-			username	= form.cleaned_data[ "username" ]
-			password	= form.cleaned_data[ "password" ]
-			user		= authenticate( username=username, password=password )
-			auth_login( request, user )
-			return HttpResponseRedirect( "/webshop/home/" )
-		#else:
-			#print "Form is not valid!"
-	else:
-		form			= AuthenticationForm( initial={ "username" : "username", "password" : "password" } )	
-	
-	next				= "/webshop/home/"
-	types				= Type.objects.all().order_by( "name" )
-	variables			= { "form" : form, "next" : next, "types" : types }
-	context				= RequestContext( request )
+	types						= Type.objects.all().order_by( "name" )
+	variables					= { "types" : types }
+	context						= RequestContext( request )
 	context.update( csrf( request ) )
 	return render_to_response( "webshop/index.html", variables, context )
 
-def category( request, type_id ):
-	type		= get_object_or_404( Type, id=type_id )
-	variables	= { "type" : type }
-	context		= RequestContext( request )
-	return render_to_response( "webshop/category.html", variables, context )
-
-def register( request ):
+def login( request ):
 	if request.method == "POST":
-		userCreationForm	= UserCreationForm( data=request.POST )
-		if userCreationForm.is_valid():
-			userCreationForm.save()
-			return HttpResponseRedirect( "/webshop/home/" )
+		authenticationForm		= AuthenticationForm( data=request.POST )
+		if authenticationForm.is_valid():
+			username			= authenticationForm.cleaned_data.get( "username", "" )
+			password			= authenticationForm.cleaned_data.get( "password", "" )
+			user				= authenticate( username=username, password=password )
+			auth_login( request, user )
+			next				= request.POST.get( "next", reverse( "webshop.views.home" ) )
+			return redirect( next )
 		#else:
 			#print "Form is not valid!"
 	else:
-		userCreationForm	= UserCreationForm()
+		authenticationForm		= AuthenticationForm( initial={ "username" : "", "password" : "" } )	
 	
-	authenticationForm		= AuthenticationForm( initial={ "username" : "username", "password" : "password" } )	
-	variables				= { "form" : authenticationForm, "userCreationForm" : userCreationForm }
-	context					= RequestContext( request )
+	next						= request.META.get( "HTTP_REFERER", reverse( "webshop.views.home" ) )
+	variables					= { "form" : authenticationForm, "next" : next }
+	context						= RequestContext( request )
+	context.update( csrf( request ) )
+	return render_to_response( "webshop/login.html", variables, context )	
+
+def category( request, type_id ):
+	type						= get_object_or_404( Type, id=type_id )
+	variables					= { "type" : type }
+	context						= RequestContext( request )
+	return render_to_response( "webshop/category.html", variables, context )
+
+def register( request ):
+	if request.user.is_authenticated():
+		return redirect( "webshop.views.account" )
+		
+	if request.method == "POST":
+		userCreationForm		= UserCreationForm( data=request.POST )
+		if userCreationForm.is_valid():
+			userCreationForm.save()
+			return redirect( "webshop.views.home" )
+		#else:
+			#print "Form is not valid!"
+	else:
+		userCreationForm		= UserCreationForm()
+	
+	variables					= { "userCreationForm" : userCreationForm }
+	context						= RequestContext( request )
 	context.update( csrf( request ) )
 	return render_to_response( "webshop/register.html", variables, context )
 
@@ -67,7 +69,7 @@ def account( request ):
 		myUserChangeForm		= MyUserChangeForm( data=request.POST, instance=request.user )
 		if myUserChangeForm.is_valid():
 			myUserChangeForm.save()
-			return HttpResponseRedirect( "/webshop/home/" )
+			return redirect( "webshop.views.home" )
 		#else:
 			#print "Form is not valid!"
 	else:
@@ -76,18 +78,16 @@ def account( request ):
 		else:
 			myUserChangeForm	= MyUserChangeForm()
 	
-	authenticationForm			= AuthenticationForm( initial={ "username" : "username", "password" : "password" } )	
-	variables					= { "form" : authenticationForm, "myUserChangeForm" : myUserChangeForm }
+	variables					= { "myUserChangeForm" : myUserChangeForm }
 	context						= RequestContext( request )
 	context.update( csrf( request ) )
 	return render_to_response( "webshop/account.html", variables, context )
 
 def logout( request ):
 	auth_logout( request )
-	return HttpResponseRedirect( "/webshop/home/" )
+	return redirect( "webshop.views.home" )
 
 def search( request ):
-	variables	= {}
-	context		= RequestContext( request )
-	return HttpResponseRedirect( "/webshop/home/" )
-#	return render_to_response( "webshop/search.html", variables, context )
+	variables					= {}
+	context						= RequestContext( request )
+	return redirect( "webshop.views.home" )
